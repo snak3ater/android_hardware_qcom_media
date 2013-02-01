83c354b46f4d9d900c6eaee94aaf7a6cc7c602a4
Paul Beeler <sparksco@gmail.com>
2013-02-01 16:14:58 -0700

    fix build:  -fno-strict-aliasing where needed
    Change-Id: I8a7c84a1ccf8b678fc9ceb10a11d7ae5341aaaca

--- mm-video-legacy/vidc/vdec/Android.mk
+++ mm-video-legacy/vidc/vdec/Android.mk
@@ -87,7 +87,7 @@ endif
 
 LOCAL_MODULE                    := libOmxVdec
 LOCAL_MODULE_TAGS               := optional
-LOCAL_CFLAGS                    := $(libOmxVdec-def)
+LOCAL_CFLAGS                    := $(libOmxVdec-def) -fno-strict-aliasing
 LOCAL_C_INCLUDES                += $(libmm-vdec-inc)
 
 LOCAL_PRELINK_MODULE    := false
@@ -117,7 +117,7 @@ mm-vdec-test-inc    += $(LOCAL_PATH)/inc
 
 LOCAL_MODULE                    := mm-vdec-omx-test
 LOCAL_MODULE_TAGS               := optional
-LOCAL_CFLAGS                    := $(libOmxVdec-def)
+LOCAL_CFLAGS                    := $(libOmxVdec-def) -fno-strict-aliasing
 LOCAL_C_INCLUDES                := $(mm-vdec-test-inc)
 
 LOCAL_PRELINK_MODULE      := false
@@ -138,7 +138,7 @@ mm-vdec-drv-test-inc    += $(LOCAL_PATH)/inc
 
 LOCAL_MODULE                    := mm-video-driver-test
 LOCAL_MODULE_TAGS               := optional
-LOCAL_CFLAGS                    := $(libOmxVdec-def)
+LOCAL_CFLAGS                    := $(libOmxVdec-def) -fno-strict-aliasing
 LOCAL_C_INCLUDES                := $(mm-vdec-drv-test-inc)
 LOCAL_PRELINK_MODULE            := false
 
